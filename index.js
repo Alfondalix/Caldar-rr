7196ceadf08403e148bf7e823d2db31172d5f842
Alfonso <alfondalix@gmail.com>
2020-11-19 14:20:01 -0300

    CAL-001: Created expess server.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const greetTeam = ()=> console.log("Hello Team");
+const express = require('express');
 
-greetTeam();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello Group</h1>');
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port: ${port}`);
+});
