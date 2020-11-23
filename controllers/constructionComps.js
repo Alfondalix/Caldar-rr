d01091e21e7cca9c1089e4e48682967aa74f5b98
Tomas Cravero <tomascravero25@gmail.com>
2020-11-23 13:01:59 -0300

    CAL-010: get resources by ID

--- controllers/constructionComps.js
+++ controllers/constructionComps.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 5000;
 app.get('/constructoras', (req, res) => {
     res.send(data);
 });
+app.get('/constructoras/:idCompany', (req, res) => {
+    const found = data.some(data => data.idCompany === parseInt(req.params.idCompany));
 
+    if (found) {
+        res.send(data.filter(data => data.idCompany === parseInt(req.params.idCompany)));
+    } else {
+        res.status(400).send({ msg: `Id not founded.` });
+    }
+});
 app.listen(port, () => console.log('corriendo en 5000'));
 
