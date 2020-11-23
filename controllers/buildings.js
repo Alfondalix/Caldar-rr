77d738b374557d57cf809fde85bdcd77dc629b63
Alfonso <alfondalix@gmail.com>
2020-11-23 15:01:43 -0300

    CAL-005: function to obtain buildings by category

--- controllers/buildings.js
+++ controllers/buildings.js
@@ -1,7 +1,35 @@
+const express = require('express');
+const router = express.Router();
 const buildings = require('../data/buildings.json');
 
-const getAllBuildings = (req, res)=>{
-    res.send(buildings)
-}
+//get all buildings
+router.get('/', (req, res)=> res.send(buildings));
 
-module.exports = getAllBuildings;
+//get building by id
+router.get('/by-id/:id', (req, res)=>{
+    const found = buildings.some(building => building.idBuilding === parseInt(req.params.id));
+    
+    if(found){
+        res.json(buildings.filter(building => building.idBuilding === parseInt(req.params.id)));
+    }
+    else{
+        res.status(400).json({ msg: `Building with the id of ${req.params.id} does not exist`})
+    }
+});
+
+// get para ver si tiene company o no, onda filtro, respondiendo a la consigna b 2.
+router.get('/by-category/:category', (req, res)=>{
+    const found = buildings.some(building => building.category === req.params.category);
+
+    if(found){
+        res.json(buildings.filter(building=> building.category === req.params.category));
+    }
+    else{
+        res.status(400).json({ msg: `Building with category ${req.params.category} does not exist`});
+    }
+})
+
+//Borrar un recurso de la lista y sobreescribir el archivo JSON para actualizarlo
+router.delete('/')
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require('express');
-const buildings = require('./data/buildings.json');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-//get all buildings
-app.get('/buildings', (req, res)=> res.send(buildings));
-
-//get building by id
-app.get('/buildings/:id', (req, res)=>{
-    res.json(buildings.filter(building=> building.idBuilding === parseInt(req.params.id)));
-});
-// get para ver si tiene company o no, onda filtro, respondiendo a la consigna b 2.
-
-//Borrar un recurso de la lista y sobreescribir el archivo JSON para actualizarlo
-
 app.get('/', (req, res)=>{
     res.send('<h1>Hello Group</h1>');
 });
+
+// Buildings routes
+app.use('/buildings', require('./controllers/buildings'));
 
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`);
