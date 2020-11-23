de88afc453a9fda06cb346d7e8a5da3be8ec52ff
giulice <giulicetto@gmail.com>
2020-11-22 23:36:03 -0300

    Delete type boyler

--- controllers/boylertypes.js
+++ controllers/boylertypes.js
@@ -6,6 +6,8 @@ const path = require('path')
 
 const boylerTypesData = require('../data/boylertypes.json')
 
+const fs = require('fs')
+
 app.get('/getAllBoylerTypes',[], (request,response) =>{
     response.json(boylerTypesData)
 })
@@ -17,7 +19,14 @@ app.get('/getBoylerTypesByName/:name',[], (request,response) =>{
 
 app.get('/getBoylerTypesById/:id',[], (request,response) =>{
     const id = request.params.id;
-    response.json(boylerTypesData.filter(boylertype => boylertype.idType == id));
+    response.json(boylerTypesData.filter(boylertype => boylertype.idType === parseInt(id)));
+})
+
+app.delete('/deleteBoylerTypes/:id',[], (request,response) =>{
+    const id = request.params.id
+    const data = JSON.parse(fs.readFileSync('data/boylertypes.json'))
+    fs.writeFileSync('data/boylertypes.json',JSON.stringify(data.filter(boylertype => boylertype.idType !== parseInt(id))))
+    response.json(data.filter(boylertype => boylertype.idType !== parseInt(id)))
 })
 
 module.exports = app
