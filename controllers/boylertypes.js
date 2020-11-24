39481e4b8a881eaf83173395beefbb37fda56b20
giulice <giulicetto@gmail.com>
2020-11-24 12:21:55 -0300

    CAL-017: fix errors and add msg

--- controllers/boylertypes.js
+++ controllers/boylertypes.js
@@ -14,12 +14,23 @@ app.get('/getAllBoylerTypes',[], (request,response) =>{
 
 app.get('/getBoylerTypesByName/:name',[], (request,response) =>{
     const name = request.params.name;
-    response.json(boylerTypesData.filter(boylertype => boylertype.name === name));
+    const found = boylerTypesData.some(boylertype => boylertype.name === name);
+    if(found) {
+        response.json(boylerTypesData.find(boylertype => boylertype.name === name));
+    } else {
+        response.status(400).send({ msg: `Name not founded.` });
+    }
 })
 
 app.get('/getBoylerTypesById/:id',[], (request,response) =>{
     const id = request.params.id;
-    response.json(boylerTypesData.filter(boylertype => boylertype.idType === parseInt(id)));
+    const found = boylerTypesData.some(boylertype => boylertype.idType === parseInt(id));
+    if(found) {
+        response.json(boylerTypesData.find(boylertype => boylertype.idType === parseInt(id)));
+    } else {
+        response.status(400).send({ msg: `Id not founded.` });
+    }
+    
 })
 
 app.delete('/deleteBoylerTypes/:id',[], (request,response) =>{
