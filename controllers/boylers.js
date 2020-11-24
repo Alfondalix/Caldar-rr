f88cc12947a5fbb53a9210c7afefd44606be3d9b
Godoy Godoy <alejandogodoy22@gmail.com>
2020-11-24 17:05:00 -0300

    CAL-003: JS code alignment fix

--- controllers/boylers.js
+++ controllers/boylers.js
@@ -1,39 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const boylers = require('../data/boylers.json');
-
 const fs = require('fs');
 
-router.get('/', (req, res)=> res.send(boylers));
+router.get('/', (req, res) => res.send(boylers));
 
 
-router.get('/by-id/:id', (req, res)=>{
+router.get('/by-id/:id', (req, res) => {
     const found = boylers.some(boyler => boyler.id === parseInt(req.params.id));
     
-    if(found){
+    if(found) {
         res.json(boylers.filter(boyler => boyler.id === parseInt(req.params.id)));
     }
-    else{
+    else {
         res.status(400).json({ msg: `Boyler with the id of ${req.params.id} not found`})
     }
 });
 
 
-router.get('/by-idType/:idType', (req, res)=>{
+router.get('/by-id-type/:idType', (req, res) => {
     const found = boylers.some(boyler => boyler.idType === parseInt(req.params.idType));
 
-    if(found){
+    if(found) {
         res.json(boylers.filter(boyler => boyler.idType === parseInt(req.params.idType)));
     }
-    else{
+    else {
         res.status(400).json({ msg: `Boyler with idType ${req.params.idType} not found`});
     }
 })
 
 
-router.delete('/delete-by-id/:id', (req, res)=>{
+router.delete('/delete-by-id/:id', (req, res) => {
     const data = JSON.parse(fs.readFileSync('data/boylers.json'));
-    fs.writeFileSync('data/boylers.json', JSON.stringify(data.filter(boyler=> boyler.id !== parseInt(req.params.id))));
+    fs.writeFileSync('data/boylers.json', JSON.stringify(data.filter(boyler => boyler.id !== parseInt(req.params.id))));
     res.json(data.filter(boyler => boyler.id !== parseInt(req.params.id)));
 })
 
