d71e3f162670e3945dde2a4bb2dc5b6db8c2557c
Alfonso <alfondalix@gmail.com>
2020-11-27 18:20:33 -0300

    CAL-002: fixed folders and files.

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const routes = require('./server/routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('<h1>Hello Group</h1>');
 });
 
 // Buildings routes
-app.use('/buildings', require('./controllers/buildings'));
+app.use('/api', routes);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
