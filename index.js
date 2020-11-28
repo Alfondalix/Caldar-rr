da8d54b4838c2d76b8868c3ddc539ff62c6b3bd1
Ignacio Sormani <ignaciosormani@gmail.com>
2020-11-28 16:31:26 -0300

    CAL-005: Refactor mongoose configuration

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const routes = require('./server/routes');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const db = require('./server/models')
-
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database");
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database", err);
-        process.exit();
-    });
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Group</h1>');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+.then(() => {
+  console.log("Connected to the database");
+})
+.catch(err => {
+  console.log("Cannot connect to the database", err);
+  process.exit();
 });
 
-// Buildings routes
 app.use('/api', routes);
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
