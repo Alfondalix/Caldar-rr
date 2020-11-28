32d5bb2a8610dbcf6ccffe076122449847cc040f
Alfonso <alfondalix@gmail.com>
2020-11-28 17:14:21 -0300

    CAL-007: install body-parser and configurate in index file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.connect(
