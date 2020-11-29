914a33dbd7a3cc2b225da80b9587ab98233fda54
alejandogodoy <alejandogodoy22@gmail.com>
2020-11-29 20:04:50 -0300

    CAL-015: Index file for the route, first look

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const boilerTypes = require('./boiler-types');
 const router = express.Router();
 
 router.use('/boiler-types', boilerTypes);
+router.use('/boilers', () => console.log('routes created'));
 
 module.exports = router;
