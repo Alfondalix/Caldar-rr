b6426c48cb70ffd2256fb962f6eff09ac4d93b93
alejandogodoy <alejandogodoy22@gmail.com>
2020-11-29 20:03:07 -0300

    CAL-015: Boilers JS file for the routes, first look

---
+++ server/routes/boilers.js
@@ -0,0 +1,20 @@
+const boiler = require("../controllers/boilers.js");
+
+var router = require("express").Router();
+
+// Retrieve all boilers
+router.get("/", boiler.findAll);
+
+// Create a new boiler
+router.post("/:id", boiler.create);
+
+//Retrieve a single boiler with id
+router.get("/:id", boiler.findOne);
+
+// Update a boiler with id
+router.put("/:id", boiler.update);
+
+// Delete a boiler with id
+router.delete("/:id", boiler.delete);
+
+module.exports = router;
