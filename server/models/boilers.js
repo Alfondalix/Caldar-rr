5d5dbca3232ff3f276f8b048529f67dce377f5ca
alejandogodoy <alejandogodoy22@gmail.com>
2020-11-30 20:30:25 -0300

    CAL-015: delete the index file (I think so) and add ref to the model of boilers

--- server/models/boilers.js
+++ server/models/boilers.js
@@ -1,19 +1,25 @@
-module.expots = mongoose => {
-    const Boilers = mongoose.model(
-        "boilers",
-        mongoose.Schema({
-            id: {
-                type: mongoose.Schema.Types.ObjectId,
-                required: true
-                
-            },
-            description: {
-                type: String,
-                required: true
-            },
-            idType:{
-                type: mongoose.Schema.ObjectId,
-                 required: true
-            },
-        }),
-    )};
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BoilersSchema = new Schema(
+    {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Boilers'
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        idType: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Boilers'
+        },
+
+});
+
+const Boilers = mongoose.model('Boilers', BoilersSchema);
+
+module.exports = Boilers;
