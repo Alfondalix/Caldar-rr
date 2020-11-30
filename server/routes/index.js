60c766e3d5c90626fd76a898f02e38e305337109
alejandogodoy <alejandogodoy22@gmail.com>
2020-11-30 20:35:52 -0300

    CAL-015: Change the var for a const for more congruence in the code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const boilerTypes = require('./boiler-types');
+const boilersRouter = require('./boilers')
 
 const router = express.Router();
 
 router.use('/boiler-types', boilerTypes);
-router.use('/boilers', () => console.log('routes created'));
+router.use('/boilers', boilersRouter);
 
 module.exports = router;
